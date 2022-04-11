86d7d16debb95193b1ba7fb1b78a732cc02dc87b
Andrii Shemshur <andriishemshur@gmail.com>
2022-04-11 09:24:15 +0200

    2

--- searching.py
+++ searching.py
@@ -19,10 +19,19 @@ def read_data(file_name, field):
         return None
     return data[field]
 
+def linear_search(sequence, number):
+    searched_res = {"positions": [], "count": 0 }
+    for idx, value in enumerate(sequence):
+        if value == number:
+            searched_res["positions"].append(idx)
+            searched_res["count"] += 1
+    return searched_res
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
-
+    results = linear_search(sequential_data, 0)
+    print(results)
 
 if __name__ == '__main__':
     main()
